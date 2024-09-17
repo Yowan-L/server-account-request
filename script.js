a6ca8ba98560729374270281fcf94bb5fcb73e41
Yowan-L <yowan.lemaire@proton.me>
2024-09-17 15:31:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// document
+//     .getElementById("registrationForm")
+//     .addEventListener("submit", function (event) {event.preventDefault();
+
+//         let username = document.getElementById("username").value;
+//         let email = document.getElementById("email").value;
+//         let password = document.getElementById("password").value;
+//         let message = document.getElementById("message");
+//         message.textContent = `Inscription réussie pour ${username} avec l'adresse ${email} !`;
+        
+//         clearForm();
+//     });
+
+//     function clearForm() {
+//     document.getElementById("username").value = "";
+//     document.getElementById("email").value = "";
+//     document.getElementById("password").value = "";
+// }
